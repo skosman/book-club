33f1f32e985f7cd131f1548723e74b10fe1b45ea
Samantha Kosman <skosman@uvic.ca>
2022-01-14 11:45:47 -0800

    Initial vue setup with router

---
+++ book-club/src/components/BookInfo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2> {{title }}</h2>
+    <h4> {{ author}} </h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'bookInfo',
+  props: {
+    title: String, 
+    author: String,
+  },
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ book-club/src/components/HelloWorld.vue
@@ -0,0 +1,52 @@
+<template>
+  <div >
+    <li v-for="book in books" :key="book.id">
+      <book-info
+        :title="book.title"
+        :author="book.author"
+      />
+    </li>
+  </div>
+</template>
+
+<script>
+import BookInfo from './BookInfo.vue'
+export default {
+  name: 'HelloWorld',
+  components: {
+    BookInfo,
+  },
+  data() {
+    return {
+      books: [
+        {
+          title: "Book",
+          author: "Test Tester",
+        },
+        {
+          title: "Book Name",
+          author: "Author Author",
+        },
+      ]
+    }
+  }  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
