6cdee7156c9a74f99708dc18fadc3fe168016dde
Samantha Kosman <skosman@uvic.ca>
2022-01-19 23:14:23 -0800

    Initialize bookstore

---
+++ bookstore/src/main/java/com/sk/bookstore/BookstoreApplication.java
@@ -0,0 +1,14 @@
+package com.sk.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+}
---
+++ bookstore/src/main/java/com/sk/bookstore/controllers/BookController.java
@@ -0,0 +1,8 @@
+package com.sk.bookstore.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+
+}
